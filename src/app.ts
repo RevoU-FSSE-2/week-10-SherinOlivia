bdf4c35a882acfaa3cc0e355ab9e4495420159f5
Sherin Olivia <133769132+SherinOlivia@users.noreply.github.com>
2023-08-25 04:40:02 +0000

    chore: misc fix. WIP: Swagger

--- src/app.ts
+++ src/app.ts
@@ -2,27 +2,26 @@ import express, {Request, Response, NextFunction} from 'express';
 import mongoMiddleware from './middleware/mongomiddleware'
 import 'dotenv/config'
 import router from './router/mainRouter';
-// import swaggerUi from 'swagger-ui-express';
-// import yaml from 'yaml';
-// import * as fs from 'node:fs';
-// import { OpenApiValidator } from 'express-openapi-validator/dist/openapi.validator';
+import swaggerUi from 'swagger-ui-express';
+import yaml from 'yaml';
+import * as fs from 'node:fs';
+import * as OpenApiValidator from 'express-openapi-validator';
 
 const port = process.env.PORT;
 const app = express()
 
 app.use(express.json())
 
-// const openAPIDoc = './doc/swagger.yaml'
-// const file = fs.readFileSync(openAPIDoc, 'utf-8')
-// const swaggerDoc = yaml.parse(file)
+const openAPIDoc = './doc/swagger.yaml'
+const file = fs.readFileSync(openAPIDoc, 'utf-8')
+const swaggerDoc = yaml.parse(file)
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
-
-// app.use(OpenApiValidator.middleware({
-//   apiSpec: openAPIDoc,
-//   validateRequests: true,
-// }))
+app.use(OpenApiValidator.middleware({
+  apiSpec: openAPIDoc,
+  validateRequests: true,
+}))
 
 app.use(mongoMiddleware)
 app.use(router)
